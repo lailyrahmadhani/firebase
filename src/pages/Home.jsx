dcec25515712319fef2482eb8d11feb97f22fabe
Laily Rahmadhani <39314731+lailyrahmadhani@users.noreply.github.com>
2022-07-29 17:32:00 +0700

    firebase

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,57 @@
+import { useEffect, useState } from "react";
 import { Card, Row, Col, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 function Home() {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let ignore = false;
+
+    async function getData() {
+      setLoading(true);
+
+      const querySnapshot = await getDocs(collection(db, "articles"));
+
+      if (ignore) return;
+
+      const articleDocs = querySnapshot.docs.map((doc) => {
+        const data = doc.data();
+        return {
+          id: doc.id,
+          ...data,
+        };
+      });
+
+      setArticles(articleDocs);
+
+      setLoading(false);
+    }
+
+    getData();
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
   return (
     <Container className="mt-3">
       <Row className="g-3">
-        {[...Array(5)].map((_, index) => (
-          <Col lg={4} key={index}>
-            <Card body>
-              <a href={`/article/${index + 1}`}>Article {index + 1}</a>
-            </Card>
-          </Col>
-        ))}
+        {loading ? (
+          <h3>Loading...</h3>
+        ) : (
+          articles.map((article) => (
+            <Col lg={4} key={article.id}>
+              <Card body>
+                <Link to={`/article/${article.id}`}>{article.title}</Link>
+              </Card>
+            </Col>
+          ))
+        )}
       </Row>
     </Container>
   );
